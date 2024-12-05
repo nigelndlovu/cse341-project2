8fc395aa93d43ec2cc8b4904cb393e7c03f96f22
nigelndlovu <ndl20004@byui.edu>
2024-12-05 09:15:35 +0200

    updates music controller

--- controllers/music.js
+++ controllers/music.js
@@ -1,4 +1,4 @@
-const mongodb = require('../data/database').default;
+const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAllMusic = async (req, res) => {
